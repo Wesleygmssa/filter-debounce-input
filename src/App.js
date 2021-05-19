34297e9f59beb273a275d56d41bc46663e0f74a0
wesley guerra <wesleyguerra9@gmail.com>
2021-05-19 17:15:08 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { DebounceInput } from "react-debounce-input";
+
 function App() {
-  return <h1>Teste</h1>;
+  const [busca, setBuscar] = useState("");
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/pessoas")
+      .then((response) => response.json())
+      .then((data) => {
+        if (busca.length >= 3) {
+          setValue(data);
+        } else {
+          setValue([]);
+        }
+      });
+  }, [busca.length]);
+
+  const lowerBusca = busca.toLowerCase();
+  const pessoasFiltradas = value.filter((pessoa) =>
+    pessoa?.name.toLowerCase().includes(lowerBusca)
+  );
+
+  return (
+    <div className="App">
+      <h1>Buscar por usuário</h1>
+      {/* <input
+        type="text"
+        value={busca}
+        onChange={(e) => setBuscar(e.target.value)}
+      /> */}
+      <DebounceInput
+        minLength={3}
+        debounceTimeout={300}
+        onChange={(event) => setBuscar(event.target.value)}
+      />
+      <ul>
+        {pessoasFiltradas.map((pessoa) => (
+          <li key={pessoa.id}>
+            {" "}
+            <a href={pessoa.id}>{pessoa.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
